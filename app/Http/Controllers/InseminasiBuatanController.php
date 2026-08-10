3943650a83767fad34bce7d2016253ba5f59a25c
Annisa Suhendar <annisasuhendar@gmail.com>
2026-08-10 10:58:19 +0700

    fix: syntax error missing semicolon and excel facade import

--- app/Http/Controllers/InseminasiBuatanController.php
+++ app/Http/Controllers/InseminasiBuatanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\InseminasiBuatan;
 use Illuminate\Http\Request;
-
+use App\Exports\InseminasiBuatanExport
 class InseminasiBuatanController extends Controller
 {
     public function index()
