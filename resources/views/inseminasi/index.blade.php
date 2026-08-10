2ac63cd0bee6f8381f646a2837f263e632e62985
Annisa Suhendar <annisasuhendar@gmail.com>
2026-08-10 11:07:33 +0700

    fix: HTML bracket syntax and export route

--- resources/views/inseminasi/index.blade.php
+++ resources/views/inseminasi/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-div class="d-flex justify-content-between align-items-center mb-3">
+<div class="d-flex justify-content-between align-items-center mb-3">
     <h2>Data Inseminasi Buatan</h2>
 
     <div>
