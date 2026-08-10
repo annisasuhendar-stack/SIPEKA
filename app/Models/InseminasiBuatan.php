7f07949eec6411a6de4860e0a007d3f77c80ffe3
Annisa Suhendar <annisasuhendar@gmail.com>
2026-08-10 11:27:29 +0700

    fix table name in InseminasiBuatan model

--- app/Models/InseminasiBuatan.php
+++ app/Models/InseminasiBuatan.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class InseminasiBuatan extends Model
 {
+    // Tambahkan baris ini untuk menentukan nama tabel secara tegas
+    protected $table = 'inseminasi_buatan';
+
     protected $fillable = [
         'jenis_hewan',
         'identitas_pemilik',
