bfd5b0f2fe2f1811be0f9b9c3be75d23437546d3
Annisa Suhendar <annisasuhendar@gmail.com>
2026-08-10 14:59:16 +0700

    add migration for pengobatans table

---
+++ database/migrations/2026_08_10_075811_create_pengobatans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('pengobatans', function (Blueprint $table) {
+        $table->id();
+        $table->string('nama_pemilik');
+        $table->string('jenis_hewan');
+        $table->string('jenis_layanan'); // Vaksinasi / Pengobatan
+        $table->string('jenis_penyakit')->nullable();
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengobatans');
+    }
+};
