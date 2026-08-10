9bad3f16b0145e86a68e3a6ed647500de4eb3fd6
Annisa Suhendar <annisasuhendar@gmail.com>
2026-08-10 12:20:14 +0700

    update model table name

--- app/Models/InseminasiBuatan.php
+++ app/Models/InseminasiBuatan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class InseminasiBuatan extends Model
 {
     // Tambahkan baris ini untuk menentukan nama tabel secara tegas
-    protected $table = 'inseminasi_buatan';
+    protected $table = 'inseminasi_buatans';
 
     protected $fillable = [
         'jenis_hewan',
